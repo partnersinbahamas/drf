a1e1460bce6548258c11739f51bc4678313aa2c6
Denys Bokov <denisbokov1703@gmail.com>
2025-09-14 16:20:44 +0200

    feat(views): create bus_list and bus_detail api_views using serliarizer

--- station/views.py
+++ station/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
+from django.http import JsonResponse
+from .models import Bus
+from .serializers import BusSerializer
 
-# Create your views here.
+
+def bus_list(request):
+    if request.method == 'GET':
+        buses = Bus.objects.all()
+        buses_serializer = BusSerializer(buses, many=True)
+
+        return JsonResponse(buses_serializer.data, safe=False, status=200)
+
+    return None
+
+def bus_detail(request, pk):
+    if request.method == 'GET':
+        bus = get_object_or_404(Bus, pk=pk)
+        bus_serializer = BusSerializer(bus)
+
+        return JsonResponse(bus_serializer.data, status=200)
+
+    return None
