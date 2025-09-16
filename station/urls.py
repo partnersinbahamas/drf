f2d4b156e88a021e4a71cb029fede61c327ec15b
Denys Bokov <denisbokov1703@gmail.com>
2025-09-16 20:05:16 +0200

    refactor(views): replace function base view with class base view

--- station/urls.py
+++ station/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import bus_list, bus_detail
+from .views import BusListView, BusDetailView
 
 app_name = 'station'
 
 urlpatterns = [
-    path('buses/', bus_list, name='bus_list'),
-    path('buses/<int:pk>/', bus_detail, name='bus_detail'),
+    path('buses/', BusListView.as_view(), name='bus_list'),
+    path('buses/<int:pk>/', BusDetailView.as_view(), name='bus_detail'),
 ]
--- station/views.py
+++ station/views.py
@@ -1,12 +1,15 @@
+from django.http import HttpRequest
+
 from rest_framework.generics import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 from .models import Bus
 from .serializers import BusSerializer
 
-
+# function base api view
 @api_view(['GET', 'POST'])
 def bus_list(request):
     if request.method == 'GET':
@@ -23,7 +26,6 @@ def bus_list(request):
             return Response(buses_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(buses_serializer.data, status=status.HTTP_201_CREATED)
-
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def bus_detail(request, pk):
@@ -43,4 +45,58 @@ def bus_detail(request, pk):
             return Response(bus_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     else:
         bus.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# class base api view
+class BusListView(APIView):
+    @staticmethod
+    def get(request: HttpRequest) -> Response:
+        buses = Bus.objects.all()
+        serializer = BusSerializer(buses, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @staticmethod
+    def post(request: HttpRequest) -> Response:
+        serializer = BusSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class BusDetailView(APIView):
+    @staticmethod
+    def get_object(pk: int) -> Bus:
+        return get_object_or_404(Bus, pk=pk)
+
+    def get(self, request: HttpRequest, pk: int) -> Response:
+        bus = self.get_object(pk)
+        serializer = BusSerializer(bus)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request: HttpRequest, pk: int) -> Response:
+        bus = self.get_object(pk)
+
+        serializer = BusSerializer(bus, data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request: HttpRequest, pk: int) -> Response:
+        bus = self.get_object(pk)
+
+        serializer = BusSerializer(bus, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request: HttpRequest, pk: int) -> Response:
+        bus = self.get_object(pk)
+        bus.delete()
+
         return Response(status=status.HTTP_204_NO_CONTENT)
