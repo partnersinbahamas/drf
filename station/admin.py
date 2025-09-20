2138b11c7e1105b06a9cc54f1a20bcbfa0b28305
Denys Bokov <denisbokov1703@gmail.com>
2025-09-20 16:04:26 +0200

    feat(models): create new models and register it in admin panel

--- station/admin.py
+++ station/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 
-from station.models import Bus
+from station.models import Bus, Facility, Trip, Ticket, Order
+
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = [TicketInLine]
 
 admin.site.register(Bus)
+admin.site.register(Facility)
+admin.site.register(Trip)
+admin.site.register(Ticket)
--- station/models.py
+++ station/models.py
@@ -1,13 +1,85 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import UniqueConstraint
+from rest_framework.exceptions import ValidationError
+
 
 class Bus(models.Model):
     info = models.CharField(max_length=255, null=True)
     num_seats = models.IntegerField()
+    facilities = models.ManyToManyField("Facility")
 
     class Meta:
         verbose_name_plural = "Buses"
         verbose_name = "Bus"
 
+    @property
+    def is_small(self):
+        return self.num_seats < 30
+
     def __str__(self):
         return f"Bus {self.info} - ({self.id})"
 
+
+class Trip(models.Model):
+    source = models.CharField(max_length=63)
+    destination = models.CharField(max_length=63)
+    departure = models.DateTimeField()
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE, related_name="trips")
+
+    def __str__(self):
+        return f"Bus({self.bus.pk}) from {self.source} to {self.destination}."
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['source', 'destination'], name='source_destination_idx'),
+            models.Index(fields=['departure'], name='departure_idx'),
+        ]
+
+
+class Ticket(models.Model):
+    seat = models.IntegerField()
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name="tickets")
+    order = models.ForeignKey("Order", on_delete=models.CASCADE, related_name="tickets")
+
+    def __str__(self):
+        return f"{self.trip} - seat: {self.seat}"
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['seat', 'trip'], name='unique_seat_trip'),
+        ]
+
+    def clean(self):
+        if self.seat < 1:
+            raise ValidationError("Seat must be greater than 1")
+
+        if self.seat > self.trip.bus.num_seats:
+            raise ValidationError("Seat must be in bus num_seats range.")
+
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return str(self.created_at)
+
+
+class Facility(models.Model):
+    name = models.CharField(max_length=85)
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+        verbose_name = "Facility"
+
+    def __str__(self):
+        return self.name
