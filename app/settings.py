f3773ce2e5e6658acec4cb4594d0558ef503a6ca
Denys Bokov <denisbokov1703@gmail.com>
2025-09-21 17:01:51 +0200

    core: enable Token authorization

--- app/settings.py
+++ app/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        # или JWTAuthentication
+    ]
+}
+
 
 # Application definition
 
