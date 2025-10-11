2d3a552fa876172cddfac4ec816c117a1e896ee3
Denys Bokov <denisbokov1703@gmail.com>
2025-10-11 15:38:05 +0200

    feat(station): extend bus image path with uuid

--- station/utils.py
+++ station/utils.py
@@ -1,6 +1,7 @@
+import uuid
 from pathlib import Path
 from django.utils.text import slugify
 
 def create_bus_image_path(bus: "Bus", filename: str) -> Path:
-    path = f"{slugify(bus.info)}" + Path(filename).suffix
+    path = f"{slugify(bus.info)}-{uuid.uuid4()}" + Path(filename).suffix
     return Path("uploads/buses/") / Path(path)
